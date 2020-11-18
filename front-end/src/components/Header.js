a732e8d951fca4bc18137fec7ff31118e1b3b46b
bevonbrye <bryebevon@gmail.com>
2020-11-18 11:46:42 -0800

    add about page with content

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -19,6 +19,8 @@ const Header = (props) => {
             <>
               <li><Link to={'/register'}>Register</Link></li>
               <li><Link to={'/login'}>Login</Link></li>
+              <li><Link to={'/About'}>About</Link></li>
+
             </>
           }
         </ul>
---
+++ front-end/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const About = () => { 
+    return ( 
+        <div>
+            <h1> hello, I work!</h1>
+         </div>
+    )
+}
+
+export default About; 
