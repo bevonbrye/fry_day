89a19f04b0aebbbb166c6b3c83b3b33d4c81cf1c
bevonbrye <bryebevon@gmail.com>
2020-11-18 21:31:40 -0800

    create components, hard code yelp data

---
+++ front-end/src/components/Restaurant.js
@@ -0,0 +1,46 @@
+import React, { components } from 'react'
+
+
+// Variable hold yelp API data: 
+
+const restaurant = {
+
+    imageSrc: 'https://https://imgur.com/XVdFqZ3.jpg',
+    name: 'Frenchies',
+    address: '42 wallaby way sydney',
+    city: 'Flavortown',
+    state: 'CA',
+    zipCode: '95354',
+    category: 'European',
+    rating: 4.5,
+    reviewCount: 90
+
+}
+
+
+class Restaurant extends React.Component { 
+  
+    render() { 
+        return(
+        <div className='Restaurant'>
+            <div className='img-container'>
+                <img src = 'https://https://imgur.com/XVdFqZ3.jpg'/>
+            </div>
+                <h2> Frenchies </h2>
+                <div className ='Restaurant-info'>
+                    <div className= 'Restaurant-address'>
+                        <p> {restaurant.address}</p>
+                        <p> {restaurant.city} </p>
+                        <p> {restaurant.state} {restaurant.zipCode} </p>
+                    </div>
+                <div className='Yelp-review'>
+                    <h3> {restaurant.category} </h3>
+                    <h3 className='rating'> {restaurant.rating} </h3>
+                    <p> {restaurant.reviewCount} </p>
+                </div>
+            </div>
+        </div>
+        )       
+  }   
+
+}
