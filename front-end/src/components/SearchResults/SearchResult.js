909fd2bf18e6ddd42792d66897922da69ca08a42
bevonbrye <bryebevon@gmail.com>
2020-11-25 23:56:56 -0800

    add to results page

--- front-end/src/components/SearchResults/SearchResult.js
+++ front-end/src/components/SearchResults/SearchResult.js
@@ -1,16 +1,44 @@
 import React from 'react'
-import { Container, Image, Col, Row } from 'react-bootstrap'
-
+import { Container, Image, Col, Row, Card, ListGroup} from 'react-bootstrap'
+import Rating from 'react-rating'
 function SearchResult() {
     return (
         <div>
-    <Container>
-        <Row>
-            <Col xs={6} md={4}>
-            <Image src="https://via.placeholder.com/150" rounded />
-            </Col>
-        </Row>
-        
+    <Container style={{marginTop:'1rem'}}>
+        <Card style={{padding:'10px'}}>
+            <Row>
+           
+                <Col> <Image src="https://via.placeholder.com/150" rounded /></Col>
+                
+                <Card style={{ width: '18rem', border:'none'}}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>  <h3> Company name </h3></ListGroup.Item>
+                        <ListGroup.Item>Address</ListGroup.Item>
+                        <ListGroup.Item style={{fontSize:'10px'}}> 
+
+                        <p>Yelp Rating: 
+                        <Rating style={{justifyContent:'space-between'}}
+                            emptySymbol={<img src="images/star-empty.png" className="icon" />}
+                            fullSymbol={<img src="/img/fries2.png" className="icon" />}
+                            />
+                        </p>
+                        <p> total reviews</p>
+                        
+                         </ListGroup.Item>
+                      
+                    </ListGroup>
+                    </Card>
+                    
+                
+              
+               
+                
+                
+                <Col>2 of 3</Col>
+                <Col>3 of 3</Col>
+            
+            </Row>
+        </Card>
     </Container>
         </div>
     )
--- front-end/src/pages/Results.js
+++ front-end/src/pages/Results.js
@@ -7,13 +7,10 @@ import SearchResult from '../components/SearchResults/SearchResult'
 export default function Results() {
     return (
         <div>
-     <Container>
-            <Row>
-                <Col><SearchResult/></Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
-            </Row>
-    </Container>
+  
+                <SearchResult/>
+                <SearchResult/>
+
         </div>
     )
 }
