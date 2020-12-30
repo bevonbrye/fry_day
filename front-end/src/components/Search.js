d8dcff7893963213568c68a6950e62bdfd3fb647
bevonbrye <bryebevon@gmail.com>
2020-12-29 18:23:46 -0800

    update all styling

--- front-end/src/components/Search.js
+++ front-end/src/components/Search.js
@@ -1,20 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.scss'
 import {Col} from 'react-bootstrap'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faSearch } from '@fortawesome/free-solid-svg-icons'
-// import { useHistory } from "react-router-dom";
 
 
-
+/* ----- Class Componant------ */
 
 
 const sortByRestaurants = {
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
 }
-
-
 
 class Search extends React.Component {
 
@@ -73,7 +68,7 @@ class Search extends React.Component {
     render() { 
         return (
             <div className="SearchBar">
-               <video src="/img/fdvid.mp4" autoPlay loop muted />
+               <video src="/img/fdvid.mp4" autoPlay loop muted style={{position:'fixed'}} />
                <h2 style={{color:'black'}}>  EVERYDAY IS FRYDAY! </h2>
                <p style={{color:'black'}}>The best website for french fry lovers.</p>
                <Col>
@@ -100,142 +95,95 @@ class Search extends React.Component {
 
 export default Search;
 
+{/* <img
+                            alt=""
+                            src="/img/ogo2.png"
+                            width="87"
+                            height="77"
+                            className="logo"
+                        /> */}
+/*---------- Hooks ------------- */
 
+// function Search (props) {
 
+// const [location, setLocation] = useState(props.location || '')
 
+   
+//   //     state = {
+//   //         term: 'fries',
+//   //         location: '',
+//   //         sortBy: 'best_match'
+//   //       };
 
+//   //       handleTermChange = handleTermChange.bind();
+//   //       handleLocationChange = handleLocationChange.bind();
+//   //       handleSearch = handleSearch.bind();
+//   //       handleSortByChange = handleSortByChange.bind();
 
+//   //       sortByRestaurant = {
+//   //         'Best Match': 'best_match',
+//   //         'Highest Rated': 'rating',
+//   //         'Most Reviewed': 'review_count'
+//   //       };
+//   // }
 
+// //  const getSortByClass = (sortByRestaurant) => { 
+// //       if(sortByRestaurant === state.sortBy) { 
+// //           return 'active';
+// //       }
+// //           return '';
+// //   }
 
+// //  const handleSortByChange = (sortByRestaurant) => { 
+// //       setState({sortByRestaurant });
+// //   }
 
-// function Search(props) {
-//   // set state to update input form  
-//    const [location, setLocation] = useState(props.location || '')
-//    let history = useHistory();
-//    const urlEncodedLocation = encodeURI(location);
+// //  const  handleTermChange = (event) => {
+// //       setState({term: event.target.value});
+// //     }
+  
+//  const handleLocationChange = (event) =>  {
+//       setLocation(event.target.value);
 
-//    function handleSubmit(e){ 
-//     history.push( `/Results?find_desc=fries&find_loc=${urlEncodedLocation}`);  
-//     //never forget: if the event does not get explicitly handled, its default action should not be taken as it normally would be.
-//      e.preventDefault();
-//    } 
-//     return (
-//         <div>
-//           {/* <Form inline style={{display:'flex', justifyContent:'center', alignItems:'center'}} >
-//       <FormControl name="Results" type="text" placeholder="Search by location" className="mr-sm-2" />
-//       <Button  onSubmit={'/Results'} name="Results" variant="outline-success" >Search</Button>
-//     </Form> */}
-
-//          <form onSubmit={handleSubmit} className='SearchBar'> 
-//               <input 
-//               // called when user does something
-//               onChange={(e) => setLocation(e.target.value)}
-//               type="text" 
-//               name="Results" 
-//               placeholder="Search by location" />
-//             <button onClick={handleSubmit} className='button'><FontAwesomeIcon icon={faSearch} /> </button>
-//         </form>
-
-        
-        
-//         </div>
-//     )
-// }
-
-
-
-// search component that enables user to choose highest rated and reviews
-// add favorite list when user is signed in 
-
-
-
-//old 
-// const sortByRestaurants = {
-//     'Highest Rated': 'rating',
-//     'Most Reviewed': 'review_count'
-// }
-
-
-
-// class Search extends React.Component {
-
-//     constructor(props) { 
-//         super(props); 
-//         this.state = {
-//             term: 'fries',
-//             location: '',
-//             sortBy: 'best_match'
-//           };
-
-//           this.handleTermChange = this.handleTermChange.bind(this);
-//           this.handleLocationChange = this.handleLocationChange.bind(this);
-//           this.handleSearch = this.handleSearch.bind(this);
-//           this.handleSortByChange = this.handleSortByChange.bind(this);
-
-//           this.sortByRestaurant = {
-//             'Best Match': 'best_match',
-//             'Highest Rated': 'rating',
-//             'Most Reviewed': 'review_count'
-//           };
+//     }
+  
+//  const handleSearch = (event) => {
+//       props.searchYelp(location);
+  
+//       event.preventDefault();
 //     }
 
-//     getSortByClass(sortByRestaurant){ 
-//         if(sortByRestaurant === this.state.sortBy) { 
-//             return 'active';
-//         }
-//             return '';
-//     }
+//   // const renderSortByRestaurants = ()  => {
+//   //     return Object.keys(sortByRestaurants).map(sortByRestaurants => {
+//   //         let sortByRestaurantValue = sortByRestaurants[sortByRestaurants]; 
+//   //         return <li key = {sortByRestaurantValue} onClick={handleSortByChange.bind( sortByRestaurantValue)} className={getSortByClass(sortByRestaurantValue)}>  {sortByRestaurants} </li>
+//   //     })
+//   // }
 
-//     handleSortByChange(sortByRestaurant) { 
-//         this.setState({sortByRestaurant });
-//     }
 
-//     handleTermChange(event) {
-//         this.setState({term: event.target.value});
-//       }
-    
-//     handleLocationChange(event) {
-//         this.setState({location: event.target.value});
-//       }
-    
-//     handleSearch(event) {
-//         this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
-    
-//         event.preventDefault();
-//       }
-
-//     renderSortByRestaurants() {
-//         return Object.keys(sortByRestaurants).map(sortByRestaurants => {
-//             let sortByRestaurantValue = sortByRestaurants[sortByRestaurants]; 
-//             return <li key = {sortByRestaurantValue} onClick={this.handleSortByChange.bind(this, sortByRestaurantValue)} className={this.getSortByClass(sortByRestaurantValue)}>  {sortByRestaurants} </li>
-//         })
-//     }
-
-//     render() { 
-//         return (
-//             <div className="SearchBar">
-//                <video src="/img/fdvid.mp4" autoPlay loop muted />
-//                <h2 style={{color:'black'}}>  EVERYDAY IS FRYDAY! </h2>
-//                <p style={{color:'black'}}>The best website for french fry lovers.</p>
-//                <Col>
-//                     <div className="SearchBar-sort-options">
-//                         <ul>
-//                             {this.renderSortByRestaurants()}
-//                         </ul>
-//                     </div>
-//                </Col>
-//                <Col>
-//                   <div className="SearchBar-fields">
-//                       <input placeholder="Search By Location" onChange={this.handleLocationChange} />
+//       return (
+//           <div className="SearchBar">
+//              <video src="/img/fdvid.mp4" autoPlay loop muted />
+//              <h2 style={{color:'black'}}>  EVERYDAY IS FRYDAY! </h2>
+//              <p style={{color:'black'}}>The best website for french fry lovers.</p>
+//              <Col>
+//                   <div className="SearchBar-sort-options">
+//                       <ul>
+//                           {/* {renderSortByRestaurants()} */}
+//                       </ul>
 //                   </div>
-//                </Col>
-                
-//                 <div className="SearchBar-submit">
-//                   <a href onClick={this.handleSearch}>Search</a>
-//                 </div>  
-//             </div>          
-//         )
-//     }
+//              </Col>
+//              <Col>
+//                 <div className="SearchBar-fields">
+//                     <input placeholder="Search By Location" onChange={handleLocationChange} />
+//                 </div>
+//              </Col>
+              
+//               <div className="SearchBar-submit">
+//                 <a href onClick={handleSearch}>Search</a>
+//               </div>  
+//           </div>          
+//       )
 // }
 
 
